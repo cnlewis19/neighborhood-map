3f449e48f725e8fc09db2b5379d442459d75edb3
Clare-Lewis <cnlewis19@gmail.com>
2018-11-11 15:38:13 -0500

    added list drawer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import restaurants from "./restaurants.json";
+import './App.css';
 import MapContainer from "./Components/map.js";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import List from './Components/list.js';
+
+library.add(faBars)
+
 
 class App extends Component {
   state = {
@@ -10,18 +18,33 @@ class App extends Component {
     all: restaurants
   }
 
+  toggleList = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  }
   render = () => {
     return (
       <div>
         <div>
+        <button onClick={this.toggleList} class="togglebutton">
+            <FontAwesomeIcon icon="bars" />
+          </button>
           <h1> Great Gluten Free in Buffalo NY </h1>
         </div>
 
         <MapContainer
+          role="application"
+          aria-label="map"
           lat={this.state.lat}
           lng={this.state.lng}
           zoom={this.state.zoom}
           locations={this.state.all} />
+
+          <List
+            locations= {this.state.all}
+            open = {this.state.open}
+            toggleList ={this.toggleList} />
       </div>
     );
   }
---
+++ src/Components/list.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Drawer from '@material-ui/core/Drawer';
+
+class List extends Component {
+  state = {
+    open: false,
+    query: ""
+  }
+  updateQuery = (newQuery) => {
+    this.setState({query: newQuery});
+  }
+
+  render = () => {
+    return (
+      <div>
+        <Drawer open={this.props.open} onClose={this.props.toggleList}>
+          <div>
+            <input
+              type="text"
+              placeholder="Filter within List"
+              name="filter"
+              onChange={e => this
+                .updateQuery(e.target.value)}
+                value={this.state.query} />
+            <ul>
+              {this.props.locations && this
+                .props
+                .locations
+                .map((location, index) => {
+                  return(
+                    <li key={index} class="drawer-list">
+                      <button key={index}>{location.name}</button>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+            </Drawer>
+          </div>
+    )
+  }
+}
+
+export default List;
